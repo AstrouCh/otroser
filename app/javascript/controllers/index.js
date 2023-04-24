07ff689f1b7ecda2c4914c0ea046852c7531da03
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-04-24 16:27:14 +0200

    implemented Nav-link's to active but doesn't work

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import DynamicNavbarController from "./dynamic_navbar_controller"
+application.register("dynamic-navbar", DynamicNavbarController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
