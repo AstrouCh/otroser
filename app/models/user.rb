bd2595bd6d98a610675336cc0cb84d6d32b78e15
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-04-28 11:15:54 +0200

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, comparison: { equal_to: "gustavo.salamie@gmail.com" }
 end
