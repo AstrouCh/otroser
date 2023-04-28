6d46668c745b22f8acf175ecb5f6a8706055d7d4
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-04-28 12:13:53 +0200

    added my email to validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email, comparison: { equal_to: "gustavo.salamie@gmail.com" }
+  validates :email, inclusion: { in: ["gustavo.salamie@gmail.com", "astridchazaux@hotmail.fr"] }
 end
